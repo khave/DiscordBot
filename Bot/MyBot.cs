bf3456ae70350b39aa4db5bf5f5d2e08dddbb712
Kristian Have <kristian.have@hotmail.com>
2017-01-20 14:43:48 +0100

    Initial Commit

---
+++ Bot/MyBot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Bot
+{
+
+    
+    class MyBot
+    {
+        DiscordClient discord;
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+            commands.CreateCommand("hello").Do(async (e) =>
+            {
+                e.Channel.SendMessage("World!");
+            });
+
+            discord.ExecuteAndWait(async () =>
+           {
+               await discord.Connect("MjcxOTg3MDQ1MTAwOTQ1NDA4.C2Oc9A.Y5ng8OYKAWbV9DRBNAl7L5mHsuI", TokenType.Bot);
+           });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Bot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+        }
+    }
+}
